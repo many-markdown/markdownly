4d6633d0c261c9caff96217220720880cae179e7
JoLeaper <joleap123@gmail.com>
2020-08-06 15:27:07 -0700

    created delete button component, added props

--- src/components/DeleteButton/DeleteButton.jsx
+++ src/components/DeleteButton/DeleteButton.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { useDispatch } from '../../hooks/appContext';
 import { deleteFile } from '../../actions/markdownActions';
 
-export default function DeleteButton() {
+export default function DeleteButton({ id }) {
   const dispatch = useDispatch();
   return (
     <div>
-      <button onClick={() => dispatch(deleteFile)}></button>
+      <button onClick={() => dispatch(deleteFile(id))}></button>
     </div>
   );
 }
+
+DeleteButton.propTypes = {
+  id: PropTypes.number.isRequired
+};
+
