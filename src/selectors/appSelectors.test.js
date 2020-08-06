f03535026a7e2ecc6859d246d7845ff09abca1bc
JoLeaper <joleap123@gmail.com>
2020-08-06 09:33:31 -0700

    getAllMarkdownFiles test written and passing

--- src/selectors/appSelectors.test.js
+++ src/selectors/appSelectors.test.js
@@ -1,4 +1,4 @@
-const { getMarkdown, getMarkdownPreview } = require('./appSelectors');
+const { getMarkdown, getMarkdownPreview, getAllMarkdownFiles } = require('./appSelectors');
 
 describe('markdown selectors', () => {
   it('returns the markdown stored in state', () => {
@@ -8,6 +8,23 @@ describe('markdown selectors', () => {
 
     const markdown = getMarkdown(state);
     expect(markdown).toEqual('# hello there');
+  });
+
+  it('returns the list of markdown files stored in state', () => {
+    const state = {
+      markdownFileList: [{
+        id: 1,
+        fileName: 'first_file',
+        body: '# hello there'
+      }]
+    };
+
+    const allMarkdownFiles = getAllMarkdownFiles(state);
+    expect(allMarkdownFiles).toEqual([{
+      id: 1,
+      fileName: 'first_file',
+      body: '# hello there'
+    }]);
   });
 
   it('creates a preview of the markdown', () => {
