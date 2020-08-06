f938cd94abfb29e31f70b22b91595ade6e9e74f7
Jpendy <Jpendy256@gmail.com>
2020-08-06 13:19:16 -0700

    added list component for files

--- src/components/fileList/FileList.jsx
+++ src/components/fileList/FileList.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { useSelector } from '../../hooks/appContext';
+import { getAllMarkdownFiles } from '../../selectors/appSelectors';
 
 export default function FileList() {
 
-  
+  const markdownFileList = useSelector(getAllMarkdownFiles);
   const listElement = markdownFileList.map(({ fileName }, i) => {
     return (
       <li key={i} >
